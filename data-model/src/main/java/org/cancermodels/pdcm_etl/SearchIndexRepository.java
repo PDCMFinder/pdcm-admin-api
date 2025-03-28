6189f6c48b4009d90445df0a54292a433a514c55
Mauricio Martinez Jimenez <mmartinez@ebi.ac.uk>
2025-03-28 15:15:33 +0000

    Deletes release related functionality
    - Release data is better handled in the dashboard

--- data-model/src/main/java/org/cancermodels/pdcm_etl/SearchIndexRepository.java
+++
@@ -1,9 +0,0 @@
-package org.cancermodels.pdcm_etl;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SearchIndexRepository extends JpaRepository<SearchIndex, Long> {
-
-}
